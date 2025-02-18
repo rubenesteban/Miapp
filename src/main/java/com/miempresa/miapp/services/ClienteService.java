19189cc2d3e554be98e2ec56980dfbd67423b337
rubenesteban <rubenmaya753@yahoo.com>
2025-02-18 10:24:55 -0500

    Update ClienteService.java

---
+++ src/main/java/com/miempresa/miapp/services/ClienteService.java
@@ -0,0 +1,27 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+    
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public List<Cliente> obtenerTodos() {
+        return clienteRepository.findAll();
+    }
+
+    public Optional<Cliente> obtenerPorId(Long id) {
+        return clienteRepository.findById(id);
+    }
+
+    public Cliente guardarCliente(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    public void eliminarCliente(Long id) {
+        clienteRepository.deleteById(id);
+    }
+}
